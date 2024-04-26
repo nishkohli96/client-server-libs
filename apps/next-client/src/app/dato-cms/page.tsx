04188dd574fd8d5e77afdfc78e15abbd48f0f426
Nishant <nishkohli96@gmail.com>
2024-04-27 02:10:53 +0530

    datocms fetching

--- apps/next-client/src/app/dato-cms/page.tsx
+++ apps/next-client/src/app/dato-cms/page.tsx
@@ -1,13 +1,26 @@
-'use client';
-
-import { ClientContext } from 'graphql-hooks';
-import { datoCmsClient } from '@/graphql/client';
-import StoreDataPage from './components/StoreData';
+import { Fragment } from 'react';
+import Link from 'next/link';
+import { PageHeading } from '@csl/shared-fe';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export default function DatoCMSPage() {
   return (
-    <ClientContext.Provider value={datoCmsClient}>
-      <StoreDataPage/>
-    </ClientContext.Provider>
+    <Fragment>
+      <PageHeading title="Dato CMS" />
+      <Typography sx={{ my: '20px' }}>
+        Fetching data from client side using
+        <Link href="https://www.npmjs.com/package/graphql-hooks" target="_blank">
+          <span> graphql hooks </span>
+        </Link>
+        and on server side using datocms api.
+      </Typography>
+      <Button variant="contained" sx={{ mr: '20px' }}>
+        <Link href="/dato-cms/client-fetch">Client side fetching</Link>
+      </Button>
+      <Button variant="outlined">
+        <Link href="/dato-cms/server-fetch">Server side fetching</Link>
+      </Button>
+    </Fragment>
   );
 }
