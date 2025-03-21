105866ef6582142ad6ae06e94e8405a5c055b760
Nishant <nishkohli96@gmail.com>
2025-03-22 00:55:32 +0530

    build & lint express & nestjs server

--- apps/nestjs-server/src/main.ts
+++ apps/nestjs-server/src/main.ts
@@ -6,7 +6,7 @@ import {
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  let a = 232;
+  let a = 1232;
   /**
    * The current config will start the NestJS application with
    * fastify adapter. To use the default express adapter, remove
