41ec580536f99920b842f6dac28e51aa6394ce0b
Nishant <nishkohli96@gmail.com>
2025-04-27 17:44:56 +0530

    firebase sign in ui done

--- apps/next-client/src/app/[locale]/firebase/auth/page.tsx
+++ apps/next-client/src/app/[locale]/firebase/auth/page.tsx
@@ -1,35 +1,44 @@
 'use client';
 
-/**
- * firebaseui library is written in UMD style (Universal Module Definition),
- * old style JS. Hence, we need to use `import * as` syntax to import it.
- */
-
 import { useEffect } from 'react';
 import firebase from 'firebase/compat/app';
-import * as firebaseUI from 'firebaseui';
-import 'firebase/compat/auth';
-import 'firebaseui/dist/firebaseui.css';
 import firebaseService from '@/services/firebase';
+import 'firebaseui/dist/firebaseui.css';
+import type firebaseui from 'firebaseui';
 
 export default function FirebaseSignIn() {
   const firebaseContainerId = 'firebase-auth-container';
-  const uiConfig = {
-    signInSuccessUrl: '/firebase',
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      // firebaseUI.auth.AnonymousAuthProvider.PROVIDER_ID
-    ]
-  };
 
+  /**
+	 * https://www.npmjs.com/package/firebaseui
+	 *
+   * firebaseui library is written in UMD style (Universal Module Definition),
+   * old style JS. Hence, we need to use `import * as` syntax to import it.
+   */
   useEffect(() => {
-    const ui = new firebaseUI.auth.AuthUI(firebaseService.getAuthInstance());
-    ui.start(`#${firebaseContainerId}`, uiConfig);
+    import('firebaseui').then(firebaseUI => {
+      const ui = firebaseUI.auth.AuthUI.getInstance() || new firebaseUI.auth.AuthUI(firebaseService.getAuthInstance());
+      /* https://www.npmjs.com/package/firebaseui#configuration */
+      const uiConfig: firebaseui.auth.Config = {
+        signInSuccessUrl: '/firebase/auth/success',
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          'apple.com',
+          'microsoft.com',
+          {
+            provider: 'yahoo.com',
+            fullLabel: 'Yahoo',
+          },
+          firebaseUI.auth.AnonymousAuthProvider.PROVIDER_ID,
+        ],
+      };
+      ui.start(`#${firebaseContainerId}`, uiConfig);
+    });
   }, []);
 
   return <div id={firebaseContainerId} />;
