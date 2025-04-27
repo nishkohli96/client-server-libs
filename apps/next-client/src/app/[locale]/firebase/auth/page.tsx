be6ba98f81031a87e725ca10b74ee5a4ba985dab
Nishant <nishkohli96@gmail.com>
2025-04-27 21:39:58 +0530

    firebase ui code

--- apps/next-client/src/app/[locale]/firebase/auth/page.tsx
+++ apps/next-client/src/app/[locale]/firebase/auth/page.tsx
@@ -17,17 +17,51 @@ export default function FirebaseSignIn() {
    */
   useEffect(() => {
     import('firebaseui').then(firebaseUI => {
-      const ui = firebaseUI.auth.AuthUI.getInstance() || new firebaseUI.auth.AuthUI(firebaseService.getAuthInstance());
+			const firebaseAuth = firebaseService.getAuthInstance();
+			const loggedUser = firebaseAuth.currentUser;
+			console.log('loggedUser: ', loggedUser);
+      const ui = firebaseUI.auth.AuthUI.getInstance() || new firebaseUI.auth.AuthUI(firebaseAuth);
+
       /* https://www.npmjs.com/package/firebaseui#configuration */
       const uiConfig: firebaseui.auth.Config = {
-        signInSuccessUrl: '/firebase/auth/success',
+				autoUpgradeAnonymousUsers: true,
+				signInSuccessUrl: '/firebase/auth/success',
         signInOptions: [
           firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          {
+						provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+						scopes: [
+							'https://www.googleapis.com/auth/contacts.readonly'
+						],
+						customParameters: {
+							/* Forces account selection. */
+							prompt: 'select_account'
+						}
+					},
+          {
+						provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+						scopes: [
+							'public_profile',
+							'email',
+							'user_likes',
+							'user_friends'
+						],
+						customParameters: {
+							/* Forces password re-entry. */
+							auth_type: 'reauthenticate'
+						}
+					},
           firebase.auth.TwitterAuthProvider.PROVIDER_ID,
           firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          {
+						provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+						recaptchaParameters: {
+							type: 'image',
+							size: 'normal',
+							badge: 'bottomleft'
+						},
+						defaultCountry: 'IN',
+					},
           'apple.com',
           'microsoft.com',
           {
@@ -36,6 +70,27 @@ export default function FirebaseSignIn() {
           },
           firebaseUI.auth.AnonymousAuthProvider.PROVIDER_ID,
         ],
+				callbacks: {
+					signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+						console.log('redirectUrl: ', redirectUrl);
+						console.log('authResult: ', authResult);
+						return true;
+					},
+					uiShown: function() {
+						const loader = document.getElementById('loader');
+            if (loader) {
+              loader.style.display = 'none';
+            }
+					},
+					// @ts-ignore
+					signInFailure: function(error) {
+						if (error.code != 'firebaseui/anonymous-upgrade-merge-conflict') {
+							return Promise.resolve();
+						}
+						const cred = error.credential;
+						return firebase.auth().signInWithCredential(cred);
+					}
+				},
       };
       ui.start(`#${firebaseContainerId}`, uiConfig);
     });
